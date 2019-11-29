64568d05eff0d8a865da1a5381b0f5803946927f
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2019-11-29 11:20:39 +0300

    Created multi-linear-regression.py

---
+++ machine-learning/machine-learning-algorithms/multi-linear-regression/multi-linear-regression.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+# linear regression model
+linear_reg = LinearRegression()
+
+# load data
+df = pd.read_csv('house-prices.txt', header=None)
+
+# rename columns
+df.columns = ['Size', 'Bedroom', 'Price']
+
+normalized_df = (df-df.mean())/df.std()
+
+# get x and y axises X = Size, y = Price
+X = np.array((normalized_df.iloc[:, 0:2]))
+y = normalized_df.Price.values.reshape(-1, 1)
+
+# find best fix prediction line
+linear_reg.fit(X, y)
+
+print(linear_reg.intercept_)
+print(linear_reg.coef_)
+
+# make prediction by using test data
+y_head = linear_reg.predict(X)
+
+
+# draw a chart to see data points and prediction line
+plt.scatter(X[:, 0:1], y, color='gray')
+plt.scatter(X[:, 1:2], y, color='red', linewidth=1)
+plt.show()
+
+
